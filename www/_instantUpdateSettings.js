c9dbb6be0da1b953020805dc5705126e3b5e530c
appmbdc <95493741+appmbdc@users.noreply.github.com>
2021-12-17 11:41:31 -0500

    Initial commit

---
+++ www/_instantUpdateSettings.js
@@ -0,0 +1,12 @@
+function _instantUpdateSettings() {
+return {
+	"baseLineGUID": "f51d3f2446e44bbc971156dc945baeaa",
+	"baseURL": "https://phonegapinstantupdate.s3.amazonaws.com/",
+	"displayMessageAfterUpdate": false,
+	"message": "",
+	"systemMessages": "Verbose",
+	"autoUpdate": true,
+	"displayFirstTimeLoadMessage": true,
+	"firstTimeRunMessage": "Please wait while the app is prepared for first-time use."
+};
+}
